1809fcf2954fa34ff8182549c257c6426c55b64b
Anastasiia <a.fediarova@gmail.com>
2023-04-30 00:20:44 +0300

    feat: add all keys

--- painter.js
+++ painter.js
@@ -14,27 +14,27 @@ h1.innerText = "RSS Virtual Keyboard";
 // const letter = document.createElement("div");
 // letter.className = "letter";
 // letter.innerText = "A";
-const key = createButton({
-  id: "",
-  ruLowCase: "S",
-  ruUpperCase: "S",
-  enLowCase: "S",
-  enUpperCase: "S",
-});
+// const key = createButton({
+//   id: "",
+//   ruLowCase: "S",
+//   ruUpperCase: "S",
+//   enLowCase: "S",
+//   enUpperCase: "S",
+// });
 
-const keyDouble = document.createElement("div");
-keyDouble.className = "key-double";
+// const keyDouble = document.createElement("div");
+// keyDouble.className = "key-double";
 
-const letterDouble = document.createElement("div");
-letterDouble.className = "letter-double";
-letterDouble.innerText = "Shift";
+// const letterDouble = document.createElement("div");
+// letterDouble.className = "letter-double";
+// letterDouble.innerText = "Shift";
 
-const keyTab = document.createElement("div");
-keyTab.className = "key-tab";
+// const keyTab = document.createElement("div");
+// keyTab.className = "key-tab";
 
-const letterTab = document.createElement("div");
-letterTab.className = "letter-tab";
-letterTab.innerText = "Tab";
+// const letterTab = document.createElement("div")
+// letterTab.className = "letter-tab";
+// letterTab.innerText = "Tab";
 
 const keyBackspace = document.createElement("div");
 keyBackspace.className = "key-backspace";
@@ -49,6 +49,23 @@ keySpace.className = "key-space";
 const letterSpace = document.createElement("div");
 letterSpace.className = "letter-space";
 
+///////Create div for keys////////////////////
+
+const divRowOne = document.createElement("div");
+divRowOne.className = "div-row-one";
+
+const divRowTwo = document.createElement("div");
+divRowTwo.className = "div-row-two";
+
+const divRowThree = document.createElement("div");
+divRowThree.className = "div-row-three";
+
+const divRowFour = document.createElement("div");
+divRowFour.className = "div-row-four";
+
+const divRowFive = document.createElement("div");
+divRowFive.className = "div-row-five";
+
 ///////////////////////////////////////////////////////////////////
 
 const p = document.createElement("p");
@@ -57,29 +74,14 @@ p.innerText = "The keyboard was created in the Windows operating system";
 document.querySelector("body").appendChild(divContainer);
 divContainer.appendChild(h1);
 divContainer.appendChild(textatea);
-divContainer.appendChild(key);
-divContainer.appendChild(
-  createButton({
-    id: "",
-    ruLowCase: "F",
-    ruUpperCase: "F",
-    enLowCase: "F",
-    enUpperCase: "F",
-  })
-);
-divContainer.appendChild(keyDouble);
-keyDouble.appendChild(letterDouble);
-
-divContainer.appendChild(keyTab);
-keyTab.appendChild(letterTab);
-
-divContainer.appendChild(keyBackspace);
-keyBackspace.appendChild(letterBackspace);
-
-divContainer.appendChild(keySpace);
-keySpace.appendChild(letterSpace);
 
 document.querySelector(".container").appendChild(p);
+
+divContainer.appendChild(divRowOne);
+divContainer.appendChild(divRowTwo);
+divContainer.appendChild(divRowThree);
+divContainer.appendChild(divRowFour);
+divContainer.appendChild(divRowFive);
 
 ////////////////////////////////////////////////////////////////
 /* {
@@ -90,15 +92,60 @@ document.querySelector(".container").appendChild(p);
     enUpperCase: "",
 
 }*/
-function createButton(obj) {
+function createButton(classes, text, data) {
   const key = document.createElement("div");
-  key.className = "key";
-
+  key.className = classes;
   const letter = document.createElement("div");
   letter.className = "letter";
-  letter.innerText = obj.ruLowCase;
+  letter.innerText = text;
   key.appendChild(letter);
-  key.setAttribute("data", JSON.stringify(obj));
+  key.setAttribute("data", JSON.stringify(data));
   return key;
 }
-console.log(divOne);
+
+divOne.forEach((obj) => {
+  if (obj.text && obj.text == "Backspace") {
+    const btn = createButton("key-backspace", obj.text, obj);
+    divRowOne.appendChild(btn);
+  } else {
+    const btn = createButton("key", obj.ruLowCase, obj);
+    divRowOne.appendChild(btn);
+  }
+});
+
+divTwo.forEach((obj) => {
+  if (obj.text && (obj.text == "Tab" || obj.text == "Del")) {
+    const btn = createButton("key-tab", obj.text, obj);
+    divRowTwo.appendChild(btn);
+  } else {
+    const btn = createButton("key", obj.ruLowCase, obj);
+    divRowTwo.appendChild(btn);
+  }
+});
+divThree.forEach((obj) => {
+  if (obj.text && (obj.text == "CapsLock" || obj.text == "Enter")) {
+    const btn = createButton("key-double", obj.text, obj);
+    divRowThree.appendChild(btn);
+  } else {
+    const btn = createButton("key", obj.ruLowCase, obj);
+    divRowThree.appendChild(btn);
+  }
+});
+divFour.forEach((obj) => {
+  if (obj.text && obj.text == "Shift") {
+    const btn = createButton("key-double", obj.text, obj);
+    divRowFour.appendChild(btn);
+  } else {
+    const btn = createButton("key", obj.ruLowCase, obj);
+    divRowFour.appendChild(btn);
+  }
+});
+divFive.forEach((obj) => {
+  if (obj.text && obj.text == "Space") {
+    const btn = createButton("key-space", "", obj);
+    divRowFive.appendChild(btn);
+  } else {
+    const btn = createButton("key", obj.text, obj);
+    divRowFive.appendChild(btn);
+  }
+});
